f2157636dab05ba2bb3c3fc3b15d53802f988230
sbani11 <65323908+sbani11@users.noreply.github.com>
2025-06-25 11:23:58 +0700

    Update regresi.js

--- regresi.js
+++ regresi.js
@@ -41,7 +41,7 @@ function calculateRegression() {
 
   document.getElementById('result').innerHTML = `
     <h3>Hasil Regresi:</h3>
-    <p>Persamaan: <strong>Y = ${a.toFixed(4)} + (${b.toFixed(4)} × ${x.toFixed(4)})<br></strong></p>
+    <p>Persamaan: <strong>Y = ${a.toFixed(4)} + (${b.toFixed(4)}X</strong></p>
   `;
 
   document.getElementById('prediction').style.display = 'block';
